8d2a965234a382defab9eafc62913f4b0a966c92
Jomar P. <jperaltac@soen.pe>
2026-09-21 22:09:59 -0500

    feat(develop): estructura inicial de springboot

---
+++ src/main/java/shopify/api/core/ShopifyApplication.java
@@ -0,0 +1,13 @@
+package shopify.api.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shopify/api/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package shopify.api.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
